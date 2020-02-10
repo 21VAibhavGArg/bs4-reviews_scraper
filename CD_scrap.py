abb643c2eb72af7950ddc1edd68bf635718f85c5
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2020-02-10 23:44:44 +0530

    minor update

--- CD_scrap.py
+++ CD_scrap.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 url = input("Enter reviews url from College Dunia: ")
 pg = int(input("Number of pages: "))
 for p in range(1, pg+1):
-    url_p = str(url) + str(p)
+    url_p = url + str(p)
     res = requests.get(url_p)
     soup = BeautifulSoup(res.text, 'html.parser')
     eles_reviews = soup.find_all(class_='content_data avatar-container')
